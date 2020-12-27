9326d3956b3859d71ac23d844efd8f3af80b5c1a
vaavaa <vaa.andrey@gmail.com>
2020-12-28 02:12:07 +0600

    pure python and pandas library solutions

--- clear_python.py
+++ clear_python.py
@@ -3,25 +3,25 @@ from timeit import default_timer as timer
 from datetime import timedelta
 
 
-# 0. 45 Code Lines
+# 0. 44 Code Lines
 # 1. Получаем год из даты которую запросили
 # 2. Прибавляем к году N лет (в целом можно хоть 1000 лет) (Это доп параметр)
 # 3. Генерим возможные варианты дат c ограничением по дню недели
 # 4. Сравниваем каждую получившуюся дату с запрошенной,
 # 5. Если дата больше, то записываем ее в итоговый массив
 # 6. Сортируем массив по возрастанию.
-# 7. Берем первый элемент из итового массива - это и есть ответ.
+# 7. Берем первый элемент из итогового массива - это и есть ответ.
 def calculate_date_faster(date_string='09.07.2010 23:36',
                           day_matrix='0,45;12;1,2,6;3,6,14,18,21,24,28;1,2,3,4,5,6,7,8,9,10,11,12;',
                           # '0,45;0,4,8,12,17,22;2,6;1,2,3,4,5,11,18,24;1,2,3,9,11;',
-                          years_count=0):
+                          years_count=1):
     start_time = timer()
-    if years_count < 0:
+    if years_count < 1:
         print('Нужно минимум +1 год. Указали {0}.'.format(years_count))
 
     date_time = convert_string_to_date(date_string)
     day_matrix = format('{0}{1}').format(day_matrix, str(date_time.year))
-    for yr in range(years_count):
+    for yr in range(years_count - 1):
         day_matrix = format('{0},{1}').format(day_matrix, str(date_time.year + yr + 1))
     day_matrix = format('{0}{1}').format(day_matrix, ';')
 
@@ -41,10 +41,9 @@ def calculate_date_faster(date_string='09.07.2010 23:36',
                                 gen_dates.append(day_date_time)
 
     gen_dates.sort()
-    next_date = gen_dates[0]
     end_time = timer()
-    print('Next date is: {0}; Elapsed time: {1}'.format(next_date, timedelta(seconds=end_time - start_time)))
-    return next_date
+    print('Next date is: {0}; Elapsed time: {1}'.format(gen_dates[0].strftime("%d.%m.%Y %H:%M"), timedelta(seconds=end_time - start_time)))
+    return gen_dates[0]
 
 
 def split_string_by_sep(string, delimiter, delimiter1):
--- pandas_python.py
+++ pandas_python.py
@@ -1,24 +1,52 @@
 from io import StringIO
 from timeit import default_timer as timer
-from datetime import timedelta
+from datetime import timedelta, datetime
 import pandas as pd
 
 
+# 0. 37 Code Lines
+# 1. Получаем год из даты которую запросили
+# 2. Прибавляем к году N лет (в целом можно хоть 1000 лет) (Это доп параметр)
+# 3. Генерим возможные варианты дат без ограничений
+# 5. Создаем доп колонку с днем недели и тип int32
+# 6. Отбираем только те которые вошли в заданные дни недели
+# 7. Ищем даты больше требуемой
+# 8. Берем первый элемент из итогового массива - это и есть ответ.
 def calculate_date_nice(date_string='09.07.2010 23:36',
                         day_matrix='0,45;12;1,2,6;3,6,14,18,21,24,28;1,2,3,4,5,6,7,8,9,10,11,12;',
-                        # '0,45;0,4,8,12,17,22;2,6;1,2,3,4,5,11,18,24;1,2,3,9,11;',
-                        years_count=0):
-    start = timer()
-    df = pd.read_csv(StringIO(day_matrix),
-                     header=None,
-                     usecols=[0, 1, 2, 3, 4], names=['minutes', 'hours', 'weekdays', 'days', 'month'],
-                     sep=';',
-                     converters={i: str for i in range(5)}).transpose()
-    minutes = pd.concat([df.iloc[0].str.split(',', expand=True)], axis=0)
-    hours = pd.concat([df.iloc[1].str.split(',', expand=True)], axis=1)
-    weekdays = pd.concat([df.iloc[2].str.split(',', expand=True)], axis=1)
-    days = pd.concat([df.iloc[3].str.split(',', expand=True)], axis=1)
-    month = pd.concat([df.iloc[4].str.split(',', expand=True)], axis=1)
+                        years_count=2):
+    start_time = timer()
+    if years_count < 1:
+        print('Нужно минимум +1 год. Указали {0}.'.format(years_count))
 
-    end = timer()
-    print(timedelta(seconds=end - start))
+    df = pd.read_csv(StringIO(day_matrix),
+                     header=None, usecols=[0, 1, 2, 3, 4], names=['minutes', 'hours', 'weekdays', 'days', 'months'],
+                     sep=';', converters={i: str for i in range(5)})
+    dfs = {}
+    for col in df:
+        dfs[col] = df[col].str.split(',', expand=True)
+    years = pd.DataFrame([pd.to_datetime(date_string, format='%d.%m.%Y %H:%M').year])
+
+    dataset = []
+    for year in years:
+        for month in dfs['months']:
+            for day in dfs['days']:
+                for hour in dfs['hours']:
+                    for minute in dfs['minutes']:
+                        dataset.append(datetime(int(years[year][0]),
+                                                int(dfs['months'][month][0]),
+                                                int(dfs['days'][day][0]),
+                                                int(dfs['hours'][hour][0]),
+                                                int(dfs['minutes'][minute][0])))
+
+    df_result = pd.DataFrame(dataset, columns=['date'])
+    df_result['weekday'] = pd.Series(df_result.date.dt.strftime("%w"), dtype='int32')
+    df_result = df_result[df_result['weekday'].isin([int(weekday) - 1 for weekday in dfs['weekdays'].iloc[0]])]
+
+    mask = (df_result['date'] > pd.to_datetime(date_string, format='%d.%m.%Y %H:%M'))
+    df_result_final = df_result.loc[mask]
+
+    end_time = timer()
+    print('Next date is: {0}; Elapsed time: {1}'.format(df_result_final.iloc[0].date.strftime("%d.%m.%Y %H:%M"),
+                                                        timedelta(seconds=end_time - start_time)))
+    return df_result_final.iloc[0]
